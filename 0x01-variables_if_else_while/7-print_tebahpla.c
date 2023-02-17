85fcb79e25ba42ef1b1ab27af87f40dd23446438
Atanda Shafau Arin-Ola <atandasharphau@gmail.com>
2023-02-17 02:37:51 -0800

    me

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ *main - Print the alphabet in return
+ *Return: Always 0
+ */
+int main(void)
+{
+int le;
+for (le = 'z'; le >= 'a'; le--)
+putchar(le);
+putchar('\n');
+return (0);
+}

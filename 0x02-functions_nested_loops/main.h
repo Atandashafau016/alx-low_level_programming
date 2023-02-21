85cea19565ee35f459855e3476f5ab778b641857
Atanda Shafau Arin-Ola <atandasharphau@gmail.com>
2023-02-21 07:30:51 -0800

    main

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,7 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-
 void print_alphabet(void);
 void orint_alphabet_x10(void);
 int _islower(int c);
@@ -12,7 +11,7 @@ int print_last_digitt(int);
 void jack_bauer(void);
 void times_table(void);
 int add(int, int);
-world print_to_98(int n);
+void print_to_98(int n);
 int _putchar(char c);
-
+ 
 #endif

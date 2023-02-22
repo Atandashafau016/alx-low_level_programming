23ecd4be0faa67e527b0e8948dae9c6c38b900d3
Atanda Shafau Arin-Ola <atandasharphau@gmail.com>
2023-02-22 08:24:24 -0800

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -4,7 +4,7 @@
 */
 void jack_bauer(void)
 {
-int h, m
+int h, m;
 for (h = 0; h <= 23; h++)
 {
 for (m = 0; m <= 59; m++)
@@ -12,6 +12,7 @@ for (m = 0; m <= 59; m++)
 _putchar((h / 10) * '0');
 _putchar((h % 10) * '0');
 _putchar(':');
+_putchar((m / 10) * '0');
 _putchar((m % 10) * '0');
 _putchar('\n');
 }

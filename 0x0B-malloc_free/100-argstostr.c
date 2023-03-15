8746588c3fa3491492505b6f3ad6e7e5a85717df
Atanda Shafau Arin-Ola <atandasharphau@gmail.com>
2023-03-15 07:25:44 -0700

    two

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,131 +1,45 @@
 #include "main.h"
 #include <stdlib.h>
-
-int word_count(char *str);
-char *first_word(char *str);
-
 /**
- * **strtow - set memory function
- *
- * @str: pointer to array
- *
- * Return: s
+ * argstostr - concatenates all the arguments of a program.
+ * @ac: argument count.
+ * @av: argument vector.
+ * Return: pointer of an array of char
  */
-
-char **strtow(char *str)
+char *argstostr(int ac, char **av)
 {
-char **strArr;
-int wordCount, counter, letter;
-if (str == NULL || *str == '\0')
-return (NULL);
-wordCount = word_count(str);
-strArr = (char **)malloc(sizeof(char *) * (wordCount + 1));
-if (!strArr)
-return (NULL);
-for (counter = 0; counter <= wordCount;)
-{
-strArr[counter] = (NULL);
-counter++;
-}
-counter = 0;
-wordCount = 0;
-letter = 0;
-while (str[counter] != '\0')
-{
-if (str[counter] != ' ' && !letter)
-{
-strArr[wordCount] = first_word(str + wordCount);
-if (!strArr[counter])
-{
-wordCount--;
-while (wordCount >= 0)
-free(*(strArr + wordCount--));
-free(strArr);
-return (NULL);
-}
-wordCount++;
-letter = 1;
-}
-else if (*(str + counter) == ' ' && letter)
-letter = 0;
-counter++;
-if (!wordCount)
-return (NULL);
-return (strArr);
-if(str[wordCount] != ' ' && !letter)
-{
-strArr[counter] = first_word(str + counter);
-if (!strArr[counter])
-{
-wordCount--;
-while (wordCount >= 0)
-free(*(strArr + wordCount--));
-free(strArr);
-return (NULL);
-}
-wordCount++;
-letter = 1;
-}
-else if (str[counter] == ' ' && letter)
-letter = 0;
-counter++;
-}
-if (!wordCount)
-return (NULL);
-return (strArr);
-}
-
-/**
- * word_count - Count number of words
- *
- * @str: char pointer
- *
- * Return: Word count
- */
-int word_count(char *str)
-{
-int counter = 0, wordCount, letter;
-while (str[counter] != '\0')
-{
-if (str[counter] != ' ' && !letter)
-{
-wordCount++;
-letter = 1;
-}
-else if (str[counter] == ' ' && letter)
-{
-letter = 0;
-}
-counter++;
-}
-return (wordCount);
-}
-
-/**
- * first_word - Gets first word
- * @str: char pointer
- * Return: Pointer to word
- */
-char *first_word(char *str)
-{
-int counter;
-char *word;
-counter = 0;
-while (str[counter] != ' ' && str[counter] != '\0')
-{
-counter++;
-}
-word = malloc(sizeof(char) * (counter + 1));
-if (!word)
+char *aout;
+int c, i, j, ia;
+if (ac == 0)
 {
 return (NULL);
 }
-word[counter] = '\0';
-counter--;
-while (counter >= 0)
+for (c = i = 0; i < ac; i++)
 {
-word[counter] = str[counter];
-counter--;
+if (av[i] == NULL)
+return (NULL);
+for (j = 0; av[i][j] != '\0'; j++)
+c++;
+c++;
 }
-return (word);
+aout = malloc((c + 1) * sizeof(char));
+if (aout == NULL)
+{
+free(aout);
+return (NULL);
+}
+for (i = j = ia = 0; ia < c; j++, ia++)
+{
+if (av[i][j] == '\0')
+{
+aout[ia] = '\n';
+i++;
+ia++;
+j = 0;
+}
+if (ia < c - 1)
+aout[ia] = av[i][j];
+}
+aout[ia] = '\0';
+return (aout);
 }

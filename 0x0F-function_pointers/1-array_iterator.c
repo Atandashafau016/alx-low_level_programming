d7e81feebe1116d25662f7aa09024ea9bdbda3ba
Atanda Shafau Arin-Ola <atandasharphau@gmail.com>
2023-03-23 08:18:43 -0700

    funtion_pointers

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,20 +1,17 @@
+#include <stdlib.h>
 #include "function_pointers.h"
-#include <stdio.h>
 /**
- * array_iterator - function that execute a function given an array
- * @array: first value
- * @size: function
- * @action: function
- *
+ * array_iterator - executes a function given as a parameter
+ * on each element of an array
+ * @array: array to iterate over
+ * @size: size of the array
+ * @action: pointer to function used
  */
-
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-size_t i;
-if (array == NULL || size == 0 || action == NULL)
+unsigned int i;
+if (!array || !action)
 return;
 for (i = 0; i < size; i++)
-{
 action(array[i]);
-}
 }
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,34 +1,35 @@
-#include <stdio.h>
-#include "function_pointers.h"
-#include <stdlib.h>
 #include "3-calc.h"
-
 /**
- * main - calculator
- * @argc: value with lenght
- * @argv: elements
- *
- * Return: always 0.
+ * main - program that perfroms simple operations
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: Always 0 (Success)
  */
-
 int main(int argc, char *argv[])
 {
-int (*function)(int, int);
-int num_1 = 0, num_2 = 0, result = 0;
+int arg1, arg2, result;
+char o;
+int (*func)(int, int);
 if (argc != 4)
 {
 printf("Error\n");
 exit(98);
 }
-function = get_op_func(argv[2]);
-if (function == NULL)
+arg1 = atoi(argv[1]);
+arg2 = atoi(argv[3]);
+func = get_op_func(argv[2]);
+if (!func)
 {
-rintf("Error\n");
+printf("Error\n");
 exit(99);
 }
-num_1 = atoi(argv[1]);
-num_2 = atoi(argv[3]);
-result = function(num_1, num_2);
-printf("%i\n", result);
+o = *argv[2];
+if ((o == '/' || o == '%') && arg2 == 0)
+{
+printf("Error\n");
+exit(100);
+}
+result = func(arg1, arg2);
+printf("%d\n", result);
 return (0);
 }

217c959b6bf24f6496ff0f7031ecaf8c7032febb
Atanda Shafau Arin-Ola <atandasharphau@gmail.com>
2023-04-12 07:19:09 -0700

    file i/o

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -11,34 +11,24 @@
  * @letters: number of letters in the file
  * Return: file and number of letters in it
  */
-
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd, count, str;
-	char *buf;
-
-	buf = malloc(sizeof(char) * letters);
-
-	if (buf == NULL)
-		return (0);
-
-	if (filename == NULL)
-		return (-1);
-
-	fd = open(filename, O_RDONLY);
-
-	if (fd < 0)
-	{
-		return (0);
-		exit(1);
-	}
-	count = read(fd, buf, letters);
-	buf[count] = '\0';
-
-	close(fd);
-
-	str = write(STDOUT_FILENO, buf, letters);
-
-
-	return (str);
+int fd, count, str;
+char *buf;
+buf = malloc(sizeof(char) * letters);
+if (buf == NULL)
+return (0);
+if (filename == NULL)
+return (-1);
+fd = open(filename, O_RDONLY);
+if (fd < 0)
+{
+return (0);
+exit(1);
+}
+count = read(fd, buf, letters);
+buf[count] = '\0';
+close(fd);
+str = write(STDOUT_FILENO, buf, letters);
+return (str);
 }

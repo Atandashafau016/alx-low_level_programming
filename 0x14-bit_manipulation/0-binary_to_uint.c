29607f4d98cc87abaf942bf7d52e4bc5529cc28d
Atanda Shafau Arin-Ola <atandasharphau@gmail.com>
2023-05-04 14:06:12 -0700

    bit manipulation

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,30 +1,48 @@
-#include "main.h"
 #include <stdio.h>
 #include <string.h>
-
+#include "main.h"
 /**
-*binary_to_uint - converts binary number to unit
-*@b: string of binary to be converted
-*Return: the converted number, or 0 if
-*there is one or more chars in the string b that is not 0 or 1
-*b is NULL
-*/
+ * _strlen - returns len of a string
+ * @str: string
+ * Return: length of the string
+ */
+unsigned int _strlen(const char *str)
+{
+	unsigned int len = 0;
 
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+	return (len);
+}
+/**
+ * binary_to_uint - Converts a binary number to an unsigned int
+ * @b: string with 0 and 1 to convert
+ * Return: converted number or 0 if there is no 0 or 1 to convert
+ */
 unsigned int binary_to_uint(const char *b)
 {
-unsigned int dec = 0;
-if (b == NULL)
-{
-return (0);
-}
-for (int i = 0; i < strlen(b); i++)
-{
-if (b[i] != '0' && b[i] != '1')
-{
-return (0);
-}
-dec <<= 1;
-dec += b[i] - '0';
-}
-return (dec);
+	unsigned int x2 = 1, conv_val = 0, len;
+	int i;
+
+	if (b == NULL)
+	{
+		return (0);
+	}
+
+	len = _strlen(b);
+	for (i = len - 1; i >= 0; i--)
+	{
+		if (b[i] != '0' && b[i] != '1')
+		{
+			return (0);
+		}
+		if (b[i] == '1')
+		{
+			conv_val += x2;
+		}
+		x2 *= 2;
+	}
+	return (conv_val);
 }

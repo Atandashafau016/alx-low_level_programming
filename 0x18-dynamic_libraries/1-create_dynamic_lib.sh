5114d631306a7ca9b546d67f6a7548e96e9345ec
Atanda Shafau Arin-Ola <atandasharphau@gmail.com>
2023-06-23 08:03:16 -0700

    create dynamic library

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-gcc -c -fPIC *.c -o *.o
-gcc -shared *.o -o liball.so
+c_files=$(find . -maxdepth 1 -type f -name "*.c")
+object_files=""
+for file in $c_files; do
+    object_file="${file%.c}.o"
+    gcc -c -fPIC "$file" -o "$object_file"
+    object_files+=" $object_file"
+done
+gcc -shared $object_files -o liball.so

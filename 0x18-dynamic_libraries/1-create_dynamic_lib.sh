ca76998789374d54090d84830ee4267b0cc1f43a
Atanda Shafau Arin-Ola <atandasharphau@gmail.com>
2023-08-06 09:50:30 -0700

    delete

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-c_files=$(find . -maxdepth 1 -type f -name "*.c")
-object_files=""
-for file in $c_files; do
-    object_file="${file%.c}.o"
-    gcc -c -fPIC "$file" -o "$object_file"
-    object_files+=" $object_file"
-done
-gcc -shared $object_files -o liball.so

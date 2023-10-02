0a7f401d96212aadc4d9bc2790565df2163ec3cc
Atanda Shafau Arin-Ola <atandasharphau@gmail.com>
2023-10-02 08:18:44 -0700

    static library

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ *_abs - computes the absolute value of an integer
+ *@t: integer to be computed
+ */
+int_abs(int t)
+{
+if (t >= 0)
+return (t);
+else
+return (-t);
+}
---
+++ 0x09-static_libraries/README.md
@@ -0,0 +1,1 @@
+C static library is a collection of object files bundled together into a single file
